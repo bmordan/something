cb4c686d49d83d9299eb18446a4677ba73c576ec
bmordan <bernardmordan@gmail.com>
2018-06-03 17:03:28 +0100

    updates chat-pane and readme

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Start with the following command
 ```
 npm start
 ```
+
+![](https://user-images.githubusercontent.com/4499581/40888502-ee483174-674f-11e8-8285-ef19eaa8a1df.jpg)
--- src/app/ChatPane.js
+++ src/app/ChatPane.js
@@ -15,7 +15,7 @@ class ChatPane extends React.Component {
   render () {
     return (
       <div className="absolute top-0 left-0 right-0 overflow-scroll" style={{bottom: '25vh'}}>
-        <div id="chat-pane" className="flex flex-column justify-end bg-near-white overflow-scroll">
+        <div id="chat-pane" className="flex flex-column justify-end bg-near-white overflow-scroll" style={{minHeight: '75vh'}}>
           {this.props.conversation.map((message, key) => {
             return (
               <div className={`ma1 pa2 ${messageClasses[message.actor]}`} key={key}>{message.text}</div>
